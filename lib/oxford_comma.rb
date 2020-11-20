df6967cea91734890a4001ee4c766bcdf151096d
Blaire <grant.blaire@gmail.com>
2020-11-20 11:29:37 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size == 3
+        return [array[0..-2].join(", "), array.last].join(", and ").to_s     
+    else
+        return [array[0..-2].join(", "), array.last].join(", and ").to_s
+    end
 end
